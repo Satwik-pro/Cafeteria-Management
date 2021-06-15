22e135b76523d6b183e004a3155af02c390c0011
Satwik-pro <satwikdash2000@gmail.com>
2021-06-15 16:17:02 +0530

    Front-End Design

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :order_items
+  resources :orders
+  resources :menu_items
+  resources :menu_categories
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
